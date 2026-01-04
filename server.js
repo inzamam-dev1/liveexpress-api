82a982ef9950b793255f4e0bb29e11548c2bc0b4
Inzamam <inzamam128@gmail.com>
2026-01-04 17:07:02 +0530

    fix dependencies

---
+++ server.js
@@ -0,0 +1,25 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// Enable CORS
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server running on port ${PORT}`);
+});
